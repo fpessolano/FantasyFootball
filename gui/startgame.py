4617b527bddbf531b0cf13a1f9d49bdedf1ddd26
Francesco Pessolano <fpessolano@users.noreply.github.com>
2021-04-07 23:38:52 +0200

    target: 0.3.1

--- gui/startgame.py
+++ gui/startgame.py
@@ -4,8 +4,8 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from support.diskstore import SaveFile
 
-
 Builder.load_file('kyvy_templates/startgame.kv')
+
 
 class StartGame(Screen):
     def __init__(self, **kwargs):
@@ -23,12 +23,12 @@ class StartGame(Screen):
     def addButton(self, *args):
         savedGames = self.saveFile.stateList()
         for savedGame in self.saveFile.stateList():
-            btn = Button(text=f'{savedGame}', font_size=24)
+            btn = Button(text=f'{savedGame}', font_size=24, background_color=[0, .7, .4, .85])
             btn.bind(on_press=self.callback)
             self.ids.saveGame.add_widget(btn)
         if len(savedGames) < 6:
-            for i in range(6-len(savedGames)):
-                btn = Button(text=f'empty', font_size=24)
+            for i in range(6 - len(savedGames)):
+                btn = Button(text=f'empty', font_size=24, background_color=[0.7, 0, 0, 0.7] )
                 self.ids.saveGame.add_widget(btn)
 
     def newGame(self):
