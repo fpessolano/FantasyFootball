51544d337c83e42e30f37185f743076770aa6eab
Francesco Pessolano <fpessolano@users.noreply.github.com>
2021-04-09 23:32:58 +0200

    target: 0.3.1

--- gui/season.py
+++ gui/season.py
@@ -3,6 +3,7 @@ import time
 
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 
 from game.team import Team
@@ -14,18 +15,27 @@ class StandingsRow(BoxLayout):
     # todo proper method for initialisation and modification
     def __init__(self, team: Team):
         super(StandingsRow, self).__init__()
-        data = team.data()
-        self.ids.name.text = data['NAME']
-        self.ids.mp.text = str(data['MP'])
-        self.ids.w.text = str(data['W'])
-        self.ids.d.text = str(data['D'])
-        self.ids.l.text = str(data['L'])
-        self.ids.gf.text = str(data['GF'])
-        self.ids.ga.text = str(data['GA'])
-        self.ids.gd.text = str(data['GD'])
-        self.ids.pt.text = str(data['PT'])
-
-
+        if team:
+            data = team.data()
+            self.ids.name.text = data['NAME']
+            self.ids.mp.text = str(data['MP'])
+            self.ids.w.text = str(data['W'])
+            self.ids.d.text = str(data['D'])
+            self.ids.l.text = str(data['L'])
+            self.ids.gf.text = str(data['GF'])
+            self.ids.ga.text = str(data['GA'])
+            self.ids.gd.text = str(data['GD'])
+            self.ids.pt.text = str(data['PT'])
+        else:
+            self.ids.name.text = 'NAME'
+            self.ids.mp.text = 'MP'
+            self.ids.w.text = 'W'
+            self.ids.d.text = 'D'
+            self.ids.l.text = 'L'
+            self.ids.gf.text = 'GF'
+            self.ids.ga.text = 'GA'
+            self.ids.gd.text = 'GD'
+            self.ids.pt.text = 'PT'
 
 
 class SeasonPlay(Screen):
@@ -38,12 +48,14 @@ class SeasonPlay(Screen):
     def load_game_data(self, data):
         # TODO
         try:
+            self.ids.title.text = 'Competition: ' + data['name'].upper()
+            self.ids.standings.add_widget(StandingsRow(None))
             for team in data['teams']:
                 # self.rows.append(self.ids.standings.add_widget(Row()))
                 self.ids.standings.add_widget(StandingsRow(team))
             print(data)
         except:
-            print ("Unexpected error:", sys.exc_info()[0])
+            print("Unexpected error:", sys.exc_info()[0])
 
     def quit(self):
         quit()
