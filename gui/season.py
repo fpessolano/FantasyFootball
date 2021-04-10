5b06adb3aafca93709efa9b6da12771c67a3b098
Francesco Pessolano <fpessolano@users.noreply.github.com>
2021-04-10 15:45:47 +0200

    target: 0.3.1

--- gui/season.py
+++ gui/season.py
@@ -12,11 +12,12 @@ Builder.load_file('kyvy_templates/season.kv')
 
 
 class StandingsRow(BoxLayout):
-    # todo proper method for initialisation and modification
-    def __init__(self, team: Team):
+    # todo proper method for modification
+    def __init__(self, team: Team = None, position=0):
         super(StandingsRow, self).__init__()
         if team:
             data = team.data()
+            self.ids.position.text = str(position)
             self.ids.name.text = data['NAME']
             self.ids.mp.text = str(data['MP'])
             self.ids.w.text = str(data['W'])
@@ -27,6 +28,7 @@ class StandingsRow(BoxLayout):
             self.ids.gd.text = str(data['GD'])
             self.ids.pt.text = str(data['PT'])
         else:
+            self.ids.name.position = '#'
             self.ids.name.text = 'NAME'
             self.ids.mp.text = 'MP'
             self.ids.w.text = 'W'
@@ -49,10 +51,10 @@ class SeasonPlay(Screen):
         # TODO
         try:
             self.ids.title.text = 'Competition: ' + data['name'].upper()
-            self.ids.standings.add_widget(StandingsRow(None))
-            for team in data['teams']:
+            self.ids.standings.add_widget(StandingsRow())
+            for i, team in enumerate(data['teams']):
                 # self.rows.append(self.ids.standings.add_widget(Row()))
-                self.ids.standings.add_widget(StandingsRow(team))
+                self.ids.standings.add_widget(StandingsRow(team, i + 1))
             print(data)
         except:
             print("Unexpected error:", sys.exc_info()[0])
