3401b974b061b3b6ab13344643fce2e5185a1f34
Francesco Pessolano <fpessolano@users.noreply.github.com>
2022-03-13 10:39:03 +0000

    fixed typo

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ main.py: game mode selector
 
 import ffmCLI
 
-print("Thanks foe playing Fantasy Manager.\nCurrently only CLI is possible")
+print("Thanks for playing Fantasy Manager.\nCurrently only CLI is possible")
 input("Press Enter to continue...")
 ffmCLI.play_game()
